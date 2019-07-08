95e5602d2108be9eb2d6607216b322fe56853c9a
andy <andylu882016@gmail.com>
2019-07-07 23:25:52 -0700

    my first commit

---
+++ eclipse-neon4.6/eclipse-jee-neon-2-win32-x86_64 (1)/MyGitHubRepository/DBDataServer/src/main/java/com/example/dbserver/DBDataServer/controller/DBServerController.java
@@ -0,0 +1,77 @@
+package com.example.dbserver.DBDataServer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.dbserver.DBDataServer.domain.*;
+import com.example.dbserver.DBDataServer.repository.CustomerRepository;
+import com.example.dbserver.DBDataServer.repository.DBRepository;
+import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/data")
+public class DBServerController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DBServerController.class);
+    
+	@Autowired
+	DBRepository dBRepository;
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@GetMapping(path="/getOne/{id}")
+	public Product getOneProduct(@PathVariable("id") final String id) {
+		logger.info("****  get one Product Item ********");
+		return dBRepository.getOneById(id);
+	}
+	
+	@GetMapping(path="/getAlls")
+	public List<Product> getAllProducts() {
+		logger.info("****  get All Product List ********");
+		return dBRepository.findAll();
+	}
+	
+	@GetMapping(path="/getOneCustomer/{id}")
+	public Customer getCustomerById(@PathVariable("id") final Integer id) {
+		logger.info("****  get One Customer ********");
+		
+		return customerRepository.getCustomerById(id);
+	}
+	
+	@GetMapping(path="/getCustomers")
+	public List<Customer> getAllCustomer() {
+	
+		logger.info("****  get All Customer List ********");
+		
+		return customerRepository.findAll();
+	}
+	@PostMapping(path="/saveOne")
+	public Customer updatedCustomer(@Validated @RequestBody Customer customer) {
+		
+		customerRepository.save(customer);
+		
+		Integer id=customer.getId();
+		
+		return  customerRepository.getCustomerById(id);
+	}
+	
+	@PostMapping(path="/saveProduct")
+	public void saveProduct(@Validated @RequestBody Product product) {
+		
+		dBRepository.save(product);
+		
+		//Integer id=customer.getId();
+		
+		//return  customerRepository.getCustomerById(id);
+	}
+	
+}
---
+++ eclipse-neon4.6/eclipse-jee-neon-2-win32-x86_64 (1)/MyGitHubRepository/DBDataServer/src/main/java/com/example/dbserver/DBDataServer/domain/Product.java
@@ -0,0 +1,74 @@
+package com.example.dbserver.DBDataServer.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="Product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	 private String id;
+	 
+	 private String name;
+	 
+	 private Integer price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Product(String id, String name, Integer price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	 
+	 
+}
